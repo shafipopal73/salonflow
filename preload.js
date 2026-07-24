5b0e7c0f37825e53d4142e371d6ead147e50d616
Zubanbaz <sagemurdar@gmail.com>
2026-07-23 23:08:56 -0400

    Switch Windows preload to CommonJS

--- preload.js
+++
@@ -1,7 +0,0 @@
-import { contextBridge, ipcRenderer } from 'electron';
-
-contextBridge.exposeInMainWorld('salonflowDesktop', {
-  showNotification: (notification) => {
-    ipcRenderer.send('show-notification', notification);
-  }
-});
